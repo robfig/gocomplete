280dc9c2a17e26af8b4a0698b724a12226bfbfb2
Rob Figueiredo <robfig@gmail.com>
2014-08-20 00:48:42 -0400

    Add some comments

--- gocomplete.sh
+++ gocomplete.sh
@@ -11,10 +11,15 @@
 # Author: Rob Figueiredo (robfig@gmail.com)
 
 function _gocomplete() {
+	# Expect the second word to be the go sub-command.
 	subcmd="${COMP_WORDS[1]}"
 	case $subcmd in
 		build|install|test)
+			# List directories with ls -d $input*/ , while ignoring stderr output
+			# Strip trailing slashes from the directory names
 			COMPREPLY=($(cd $GOPATH/src && ls -d $2*/ 2>/dev/null | sed 's#/$##g'))
+			
+			# If there is only match, jump right into the completions for that one.
 			if [[ ${#COMPREPLY[@]} == 1 ]]; then
 				COMPREPLY=($(cd $GOPATH/src && ls -d ${COMPREPLY[0]}/*/ 2>/dev/null | sed 's#/$##g'))
 			fi
@@ -24,4 +29,6 @@ function _gocomplete() {
 	return 1
 }
 
+# -o default falls back to default matching if we found nothing
+# -o nospace strips trailing space from the completion
 complete -o default -o nospace -F _gocomplete go
